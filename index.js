1d18d32ab5f12a2ba408680d85139660d025255d
NickMoignard <nicklmoignard@gmail.com>
2020-10-15 00:20:31 +1100

    added command stubs

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-// import { NODE_ENV } from './config';
+const startsWith = (msg, command) => {
+    return msg.content.startsWith(command);
+};
+const help = (msg) => {
+    msg.reply("unfinished function");
+};
+const newClient = (msg) => {
+    msg.reply("unfinished function");
+};
+const newCampaign = (msg) => {
+    msg.reply("unfinished function");
+};
+
+const checkForCommand = (msg) => {
+    var commands = [
+        ['!help', help],
+        ['!new-client', newClient],
+        ['!new-campaign', newCampaign]
+    ];
+
+    commands.forEach((command) => {
+        if (startsWith(msg, command[0])) {
+            command[1](msg);
+        }
+    });
+};
+
+const initializeRVLT6900 = () => {
+    // DO STUFF
+};
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-const initializeRVLT6900 = () => {
-
-};
-
-const checkMessage = (msg) => {
-    if (msg.content === "ping") {
-        msg.reply("Bong!");
-    }
-};
-
-client.on("ready", initializeRVLT6900).on("message", checkMessage);
+require('dotenv').config()
+client.on("ready", initializeRVLT6900).on("message", checkForCommand);
 client.login(process.env.TOKEN);
